747fd1dbfd026f901c41ca9eefc0c981b582d9e2
timetherewere <mikjosjon@gmail.com>
2022-01-26 20:04:17 -0500

    Print current scroll position for debugging.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 console.log("Running script");
 
+const printScrollHeight = function(){
+  let currentScrollHeight = window.scrollY;
+  console.log("Current scroll position: " + currentScrollHeight);
+}
+
+// Force the scroll bar to default to the BOTTOM of the page
 let windowHeight = window.innerHeight;
 const siteContainer = document.getElementById("site_container");
 let siteHeight = siteContainer.clientHeight;
-console.log("siteHeight: " + siteHeight);
-console.log("windowHeight: " + windowHeight);
 window.scroll(0, siteHeight - windowHeight);
-console.log("scrolled to: " + (siteHeight - windowHeight));
+document.addEventListener("scroll", printScrollHeight);
