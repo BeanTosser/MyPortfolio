195af4a5d97808739afe56f013e2a73855d14ec4
timetherewere <mikjosjon@gmail.com>
2022-01-27 20:17:07 -0500

    Refactor design; Revert to standard top-to-bottom flow.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,76 @@
-console.log("Running script");
+const MOBILE_WIDTH_THRESHOLD = 500;
 
-const printScrollHeight = function(){
+let isMobile = false;
+
+let sectionHeights = [];
+let navBar;
+
+const printScrollHeight = function () {
   let currentScrollHeight = window.scrollY;
   console.log("Current scroll position: " + currentScrollHeight);
+  console.log("current section: " + window.location.pathname + "     " + window.location.hash);
+};
+
+window.onresize = () => {
+  if(window.innerWidth < MOBILE_WIDTH_THRESHOLD && !isMobile){
+    isMobile = true;
+    updateNavBar();
+  } else if(window.innerWidth >= MOBILE_WIDTH_THRESHOLD && isMobile){
+    isMobile = false;
+    updateNavBar();
+  }
 }
 
-// Force the scroll bar to default to the BOTTOM of the page
-let windowHeight = window.innerHeight;
-const siteContainer = document.getElementById("site_container");
-let siteHeight = siteContainer.clientHeight;
-window.scroll(0, siteHeight - windowHeight);
-document.addEventListener("scroll", printScrollHeight);
+function updateNavBar(){
+  console.log("Updating nav bar");
+  console.log("Old innerHTML: " + navBar.innerHTML)
+  if(isMobile){
+    navBar.innerHTML = `
+      <img src="./images/Burger.png" alt="Burger Menu" id="burger_menu" />  
+    `
+  } else {
+    navBar.innerHTML = `
+      <nav id="desktop_nav_links">
+        <ul>
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#work">Work</a>
+          <a href="#contact">Contact</a>
+        </ul>
+      </nav>  
+    `
+  }
+  console.log("New navbar html: " + navBar.innerHTML);
+}
+
+window.onload = () => {
+  // Check if the window width indicates this is likely a mobile device
+  if(window.innerWidth < MOBILE_WIDTH_THRESHOLD){
+    isMobile = true;
+  }
+  
+  navBar = document.getElementById("nav_bar");
+  updateNavBar();
+  
+  const siteContainer = document.getElementById("site_container");
+  let siteHeight = siteContainer.clientHeight;
+
+  const sections = document.getElementsByClassName("section");
+  sectionHeights = Array.prototype.map.call(sections, (section, index) => {
+    let sectionName;
+    switch(index){
+      case 0:
+        sectionName = "Landing";
+        break;
+      case 1:
+        sectionName = "About";
+        break;
+      default:
+        sectionName = "Error";
+    }
+    console.log("section height: " + section.clientHeight);
+    return {name: sectionName, height: section.clientHeight};
+  })
+
+  document.addEventListener("scroll", printScrollHeight);
+};
